9b7efcbcbc92a35e8dd5ea2dd1bbfe1b2d2058dc
matias <matiasluissosa@hotmail.com>
2025-09-28 22:57:45 -0300

    4

--- A entregar/Pareja2.R
+++ A entregar/Pareja2.R
@@ -1,5 +1,5 @@
-# TP1 - Inferencia Estadística y Reconocimiento de Patrones
-# Parte de regresión en R
+# TP1 - Inferencia EstadC-stica y Reconocimiento de Patrones
+# Parte de regresiC3n en R
 
 #Cargo las librerias
 library(tidyverse)
@@ -10,7 +10,7 @@ library(glmnet)
 rm(list = ls())
 
 #Cargamos el dataset
-vinos <- read.csv2("C:\Users\Matias\OneDrive\Escritorio\INFERENCIA ESTADISTICA\TP 1\winequality-red.csv",dec=".")
+vinos <- read.csv2("winequality-red.csv",dec=".")
 
 #Miramos los nombres de las variables y vemos una vista rapida de la estructura
 #del dataset
@@ -18,14 +18,14 @@ names(vinos)
 head(vinos)
 attach(vinos)
 
-# Estadísticas descriptivas de cada variable
+# EstadC-sticas descriptivas de cada variable
 describe <- summary(vinos)
 print(describe)
 
-# Matriz de correlación y dispersión
+# Matriz de correlaciC3n y dispersiC3n
 corrplot(cor(vinos), method = 'color', tl.cex = 0.7)
 cor_matrix <- cor(vinos)
-ggpairs(vinos, columns = 1:12, title = "Matriz de correlaciones y dispersión")
+ggpairs(vinos, columns = 1:12, title = "Matriz de correlaciones y dispersiC3n")
 cor(vinos$quality, vinos[, 1:12])
 
 # Separamos las variables explicativas de la variable respuesta
@@ -39,34 +39,34 @@ train_idx <- sample(seq_len(nrow(vinos)), size = 0.8 * nrow(vinos))
 train <- vinos[train_idx, ]
 test <- vinos[-train_idx, ]
 
-#Creo la matriz de diseño y vector de respuestas 
+#Creo la matriz de diseC1o y vector de respuestas 
 X_train <- as.matrix(train[, !(names(train) %in% c('quality'))])
 y_train <- train$quality
 X_test <- as.matrix(test[, !(names(test) %in% c('quality'))])
 y_test <- test$quality
 
-# --- Regresión lineal múltiple ---
+# --- RegresiC3n lineal mC:ltiple ---
 lm_fit <- lm(quality ~ ., data = train)
 # Hacemos un summary del modelo
 summary(lm_fit)
 
-# El modelo lineal múltiple es estadísticamente significativo (p < 2.2e-16),
+# El modelo lineal mC:ltiple es estadC-sticamente significativo (p < 2.2e-16),
 # lo que indica que al menos una variable predictora tiene
-# asociación con la calidad del vino. El R² es de 0.3536, lo que implica que
+# asociaciC3n con la calidad del vino. El RB2 es de 0.3536, lo que implica que
 # el modelo explica aproximadamente el 35% de la variabilidad en la respuesta.
 
-# Las variables con mayor evidencia estadística (p < 0.001) son alcohol, 
+# Las variables con mayor evidencia estadC-stica (p < 0.001) son alcohol, 
 # volatile y sulphates.
 
-# Las variables con poca evidencia estadística (p > 0.05) son:
+# Las variables con poca evidencia estadC-stica (p > 0.05) son:
 # fixed.acidity, citric.acid, residual.sugar, density, pH y free.sulfur.dioxide.
 
 # Calculamos los predichos del conjunto de prueba
 lm_pred <- predict(lm_fit, newdata = test)
 
-# Metricas de evaluación del modelo 
-cat('Regresión lineal múltiple:\n')
-cat('Error Cuadrático Medio (MSE):', mean((test$quality - lm_pred)^2), '\n')
+# Metricas de evaluaciC3n del modelo 
+cat('RegresiC3n lineal mC:ltiple:\n')
+cat('Error CuadrC!tico Medio (MSE):', mean((test$quality - lm_pred)^2), '\n')
 cat('R2:', summary(lm_fit)$r.squared, '\n')
 
 # Graficos de residuos
@@ -78,10 +78,10 @@ ajustados <- fitted(lm_fit)
 hist(residuos,
      breaks = 30,
      col = "steelblue",
-     main = "Distribución de residuos",
+     main = "DistribuciC3n de residuos",
      xlab = "Residuos")
 
-# Gráfico de dispersión
+# GrC!fico de dispersiC3n
 plot(ajustados, residuos,
      xlab = "Valores ajustados",
      ylab = "Residuos",
@@ -95,10 +95,10 @@ cv_ridge <- cv.glmnet(X_train, y_train, alpha = 0)
 best_lambda_ridge <- cv_ridge$lambda.min
 ridge_pred <- predict(cv_ridge, s = best_lambda_ridge, newx = X_test)
 
-# Metricas de evaluación del modelo 
+# Metricas de evaluaciC3n del modelo 
 cat('\nRidge:\n')
 r2_ridge <- 1 - sum((y_test - ridge_pred)^2) / sum((y_test - mean(y_test))^2)
-cat("Error Cuadrático Medio (MSE): ", mean((y_test - ridge_pred)^2))
+cat("Error CuadrC!tico Medio (MSE): ", mean((y_test - ridge_pred)^2))
 cat('R2:', r2_ridge, '\n')
 
 # Graficos de residuos
@@ -124,10 +124,10 @@ cv_lasso <- cv.glmnet(X_train, y_train, alpha = 1)
 best_lambda_lasso <- cv_lasso$lambda.min
 lasso_pred <- predict(cv_lasso, s = best_lambda_lasso, newx = X_test)
 
-# Metricas de evaluación del modelo 
+# Metricas de evaluaciC3n del modelo 
 r2_lasso <- 1 - sum((y_test - lasso_pred)^2) / sum((y_test - mean(y_test))^2)
 cat('\nLASSO:\n')
-cat("Error Cuadrático Medio (MSE): ", mean((y_test - lasso_pred)^2))
+cat("Error CuadrC!tico Medio (MSE): ", mean((y_test - lasso_pred)^2))
 cat('R2:', r2_lasso, '\n')
 
 # Graficos de residuos
